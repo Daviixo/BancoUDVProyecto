e2cce6a09c35c6bc1494a4d58815fc09e7bdc828
PTote <71539041+PTote@users.noreply.github.com>
2021-05-09 13:33:04 -0600

    updateUsers

--- BancoAplicaciones/Bank_UDV_30032021/updateUsers.php
+++ BancoAplicaciones/Bank_UDV_30032021/updateUsers.php
@@ -7,51 +7,81 @@
     <title>Document</title>
 </head>
 <body>
-<form>
-  <div class="form-row">
-    <div class="form-group col-md-6">
-      <label for="inputEmail4">Email</label>
-      <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+    <form method="post" id="formulario">
+    <input type="text" name="dpi" id="dpi">
+    <input type="submit" value="search">
+    </form>
+
+    <h2>Información de cliente.</h2>
+    <div >
+    <table>
+        <thead>
+            <tr>
+                <th>campo</th>
+                <th>descripción</th>
+            </tr>
+        </thead>
+        <tbody id="dataCliente">
+        </tbody>
+    </table>
     </div>
-    <div class="form-group col-md-6">
-      <label for="inputPassword4">Password</label>
-      <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
-    </div>
-  </div>
-  <div class="form-group">
-    <label for="inputAddress">Address</label>
-    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
-  </div>
-  <div class="form-group">
-    <label for="inputAddress2">Address 2</label>
-    <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
-  </div>
-  <div class="form-row">
-    <div class="form-group col-md-6">
-      <label for="inputCity">City</label>
-      <input type="text" class="form-control" id="inputCity">
-    </div>
-    <div class="form-group col-md-4">
-      <label for="inputState">State</label>
-      <select id="inputState" class="form-control">
-        <option selected>Choose...</option>
-        <option>...</option>
-      </select>
-    </div>
-    <div class="form-group col-md-2">
-      <label for="inputZip">Zip</label>
-      <input type="text" class="form-control" id="inputZip">
-    </div>
-  </div>
-  <div class="form-group">
-    <div class="form-check">
-      <input class="form-check-input" type="checkbox" id="gridCheck">
-      <label class="form-check-label" for="gridCheck">
-        Check me out
-      </label>
-    </div>
-  </div>
-  <button type="submit" class="btn btn-primary">Sign in</button>
-</form>
+
+    <script>
+
+    let template= ({...json})=>`
+    
+   
+            <tr>
+                <td>Nombre</td>
+                <td>${ json.cliente_nombre1} ${json.cliente_ape1} ${json.cliente_ape2}</td>
+            </tr>
+            <tr>
+                <td>DPI</td>
+                <td>${json.cliente_ide}</td>
+            </tr>
+           
+       
+    
+    `
+
+    let formulario=document.querySelector("#formulario");
+    formulario.addEventListener("submit",function(e){  
+        e.preventDefault() 
+    let dpi=document.querySelector("#dpi");
+    console.log(dpi.value);
+    let infocliente=document.querySelector("#dataCliente");
+    fetch("https://banco-vivienda.club/clientes/consulta/"+dpi.value, {
+    "headers": {
+        'Accept': 'application/json',
+        "authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZFN1Y3Vyc2FsIjo1LCJ1c2VybmFtZSI6IlNVQ1VSU0FMMSIsInBhc3N3b3JkIjoicXdlcnR5IiwiaWF0IjoxNjE5NTQ3NzQ0LCJleHAiOjE2MjI2NTgxNDR9.1awdMkX9_Ajun1OLcYXD19_UbtKVgx4Uzbmy55Jlrt4"
+    }
+})
+    .then(response => {
+    
+            let respuestaServer= response.json().then(respuesta=>{ 
+                return respuesta;
+            }) .catch(error=>{
+                alert ("El número de DPI no existe en la base de datos.");
+                infocliente.innerHTML="";
+            })
+      return respuestaServer;
+       
+    })
+    .then(response => {
+        console.log(response);
+        
+        infocliente.innerHTML=template(response);
+        console.log(response.ok)
+        
+    })
+    .catch(err => {
+        console.error(err);
+    });
+
+    })
+
+   
+    
+    </script>
 </body>
 </html>

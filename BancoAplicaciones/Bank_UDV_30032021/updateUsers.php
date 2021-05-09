dd4693da29b15089e5b8493a4141622b1ee069e1
PTote <71539041+PTote@users.noreply.github.com>
2021-05-09 14:14:18 -0600

    delete

--- BancoAplicaciones/Bank_UDV_30032021/updateUsers.php
+++ BancoAplicaciones/Bank_UDV_30032021/updateUsers.php
@@ -7,10 +7,7 @@
     <title>Document</title>
 </head>
 <body>
-    <form method="post" id="formulario">
-    <input type="text" name="dpi" id="dpi">
-    <input type="submit" value="search">
-    </form>
+   
 
     <form  method="post" id="formularioDelete">
     <input type="text" name="dpi" id="dpi">
@@ -31,60 +28,6 @@
     </table>
     </div>
 
-    <script>
-
-    let template= ({...json})=>`
-    
-   
-            <tr>
-                <td>Nombre</td>
-                <td>${ json.cliente_nombre1} ${json.cliente_ape1} ${json.cliente_ape2}</td>
-            </tr>
-            <tr>
-                <td>DPI</td>
-                <td>${json.cliente_ide}</td>
-            </tr>
-           
-       
-    
-    `
-
-    let formulario=document.querySelector("#formulario");
-    formulario.addEventListener("submit",function(e){  
-        e.preventDefault() 
-    let dpi=document.querySelector("#dpi");
-    console.log(dpi.value);
-    let infocliente=document.querySelector("#dataCliente");
-    fetch("https://banco-vivienda.club/clientes/consulta/"+dpi.value, {
-    "headers": {
-        'Accept': 'application/json',
-        "authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZFN1Y3Vyc2FsIjo1LCJ1c2VybmFtZSI6IlNVQ1VSU0FMMSIsInBhc3N3b3JkIjoicXdlcnR5IiwiaWF0IjoxNjE5NTQ3NzQ0LCJleHAiOjE2MjI2NTgxNDR9.1awdMkX9_Ajun1OLcYXD19_UbtKVgx4Uzbmy55Jlrt4"
-    }
-})
-    .then(response => {
-    
-            let respuestaServer= response.json().then(respuesta=>{ 
-                return respuesta;
-            }) .catch(error=>{
-                alert ("El número de DPI no existe en la base de datos.");
-                infocliente.innerHTML="";
-            })
-      return respuestaServer;
-       
-    })
-    .then(response => {
-        console.log(response);
-        
-        infocliente.innerHTML=template(response);
-        console.log(response.ok)
-        
-    })
-    .catch(err => {
-        console.error(err);
-    });
-
-    })
-    </script>
 
 <script>
 

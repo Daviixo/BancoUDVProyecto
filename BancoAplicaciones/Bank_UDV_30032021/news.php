a1a8da9ffda52a4a4586216a444dff0c2fd13512
David Díaz Veras <daviidvx93@gmail.com>
2021-05-09 18:06:12 -0600

    testing again

--- BancoAplicaciones/Bank_UDV_30032021/news.php
+++ BancoAplicaciones/Bank_UDV_30032021/news.php
@@ -172,41 +172,31 @@ if (isset($_POST['logout'])) {
 
         <!--INICIO DE FORMULARIO PARA CONSULTA-->
 
-            <form method="post" id="formulario">
-                <!--<input type="text" name="dpi" id="dpi">-->
-                <input type="submit" value="search">
 
-            </form>
+        <form method="post" id="formulario">
+            <input type="text" name="dpi" id="dpi">
+            <input type="submit" value="search">
+        </form>
+        <input type="text" name="mostrarNombre" id="mostrarNombre">
+        <h2>Información de cliente.</h2>
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>DPI</th>
+                        <th>Nombre</th>
+                        <th>Apellido</th>
+                    </tr>
+                </thead>
+                <tbody id="dataCliente">
+                </tbody>
+            </table>
+        </div>
 
-            <form>
-            
-            <input type="text" id="dpiFinal" placeholder="DPI" maxlength="13">
-
-            </form>
-
-            <h2>Información de cliente.</h2>
-            <div>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>DPI</th>
-                            <th>Nombre</th>
-                            <th>Apellido</th>
-                        </tr>
-                    </thead>
-                    <tbody id="dataCliente">
-                    </tbody>
-                </table>
-            </div>
-
-            <script>
-
-            var dpiFinal = document.getElementById("dpiFinal").value;
-
-            console.log("El DPI consultado es: " + dpiFinal);
-                let template = ({
-                    ...json
-                }) => `
+        <script>
+            let template = ({
+                ...json
+            }) => `
     
             
             <tr>
@@ -215,44 +205,48 @@ if (isset($_POST['logout'])) {
 				<td>${json.cliente_ape1}</td>
 				
             </tr>
+            
+           
+       
+    
     `
-                
-                let formulario = document.querySelector("#formulario");
-                formulario.addEventListener("submit", function(e) {
-                    e.preventDefault()
-                    //let dpi = document.querySelector("#dpi");
-                    //console.log(dpi.value);
-                    //let infocliente = document.querySelector("#dataCliente");
-                    fetch("https://banco-vivienda.club/clientes/consulta/" + dpiFinal, {
-                            "headers": {
-                                'Accept': 'application/json',
-                                "authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZFN1Y3Vyc2FsIjo1LCJ1c2VybmFtZSI6IlNVQ1VSU0FMMSIsInBhc3N3b3JkIjoicXdlcnR5IiwiaWF0IjoxNjE5NTQ3NzQ0LCJleHAiOjE2MjI2NTgxNDR9.1awdMkX9_Ajun1OLcYXD19_UbtKVgx4Uzbmy55Jlrt4"
-                            }
+
+            let formulario = document.querySelector("#formulario");
+            formulario.addEventListener("submit", function(e) {
+                e.preventDefault()
+                let dpi = document.querySelector("#dpi");
+                console.log(dpi.value);
+                let infocliente = document.querySelector("#dataCliente");
+                fetch("https://banco-vivienda.club/clientes/consulta/" + dpi.value, {
+                        "headers": {
+                            'Accept': 'application/json',
+                            "authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZFN1Y3Vyc2FsIjo1LCJ1c2VybmFtZSI6IlNVQ1VSU0FMMSIsInBhc3N3b3JkIjoicXdlcnR5IiwiaWF0IjoxNjE5NTQ3NzQ0LCJleHAiOjE2MjI2NTgxNDR9.1awdMkX9_Ajun1OLcYXD19_UbtKVgx4Uzbmy55Jlrt4"
+                        }
+                    })
+                    .then(response => {
+
+                        let respuestaServer = response.json().then(respuesta => {
+                            return respuesta;
+                        }).catch(error => {
+                            alert("El número de DPI no existe en la base de datos.");
+                            infocliente.innerHTML = "";
                         })
-                        .then(response => {
+                        return respuestaServer;
 
-                            let respuestaServer = response.json().then(respuesta => {
-                                return respuesta;
-                            }).catch(error => {
-                                alert("El número de DPI no existe en la base de datos.");
-                                infocliente.innerHTML = "";
-                            })
-                            return respuestaServer;
+                    })
+                    .then(response => {
+                        console.log(response);
 
-                        })
-                        .then(response => {
-                            console.log(response);
+                        infocliente.innerHTML = template(response);
+                        console.log(response.ok)
 
-                            infocliente.innerHTML = template(response);
-                            console.log(response.ok)
+                    })
+                    .catch(err => {
+                        console.error(err);
+                    });
 
-                        })
-                        .catch(err => {
-                            console.error(err);
-                        });
-
-                })
-            </script>
+            })
+        </script>
 
         <!--FINAL DE FORMULARIO-->
 

52efa23cbe8f219d954925a91e5979e48cff750e
David Díaz Veras <daviidvx93@gmail.com>
2021-05-10 15:06:00 -0600

    trying again

--- BancoAplicaciones/Bank_UDV_30032021/historial.php
+++ BancoAplicaciones/Bank_UDV_30032021/historial.php
@@ -1,47 +1,31 @@
 <?php
 
-    echo $_GET['creditid'];
-    echo $_GET['amounttopay'];
-
     function updateHistorialPago($finalid,$finalamount){
         
-        $_SERVER = "localhost";
-        $username = "usuario";
-        $password = "DavincianosA*2021a";
-        $database = "bancoaplicaciones";
-        $today = date("F j, Y, g:i a");
-        $credit_id = $_GET['creditid'];
-        $credit_amount = $_GET['amounttopay'];
-
-        $action = "Credit payment";
-        $email = $_SESSION['username'];
-
-        var_dump($credit_id);
-        var_dump($credit_amount);
-
-        // if(isset($credit_id) && isset($credit_amount)){
-        //     echo "ID y amount van NULL";
-        //     exit();
-        // }
-
-        echo $credit_id + ' > Credit ID\n';
-        echo $credit_amount + ' > Credit amount';
-
-        //$final_credit_id = strval($credit_id);
-        //$final_credit_amount = strval($credit_amount);
-
-        //$conex = mysqli_connect($_SERVER, $username, $password, $database);
-
         try {
+
+            $_SERVER = "localhost";
+            $username = "usuario";
+            $password = "DavincianosA*2021a";
+            $database = "bancoaplicaciones";
+            $today = date("F j, Y, g:i a");
+            $credit_id = $_GET['creditid'];
+            $credit_amount = $_GET['amounttopay'];
+    
+            $action = "Credit payment";
+            $email = $_SESSION['username'];
+    
+            var_dump($credit_id);
+            var_dump($credit_amount);
+    
+            echo $credit_id + ' > Credit ID\n';
+            echo $credit_amount + ' > Credit amount';
+    
             $opciones = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
             $conn = new PDO("mysql:host=$_SERVER;dbname=$database", $username, $password, $opciones);
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            echo "Connected to $database successfully.";
-        } catch (PDOException $e) {
-            die("connected failed:" . $e->getMessage());
-        }
 
-        try {
+            echo "Connected to $database successfully.";
             echo "---\n";
             var_dump($credit_id);
             var_dump($credit_amount);
@@ -67,5 +51,3 @@
     }
     
     updateHistorialPago($_GET['creditid'],$_GET['amounttopay']);
-
-?>

3fb0028b03c02a9a3abfef9a6d4b435990a8189a
David Díaz Veras <daviidvx93@gmail.com>
2021-05-10 13:10:30 -0600

    testing query

--- BancoAplicaciones/Bank_UDV_30032021/services.php
+++ BancoAplicaciones/Bank_UDV_30032021/services.php
@@ -162,10 +162,6 @@ if (isset($_POST['logout'])) {
             </div><!-- /.container -->
         </section><!-- /.page-header -->
 
-        <?php
-        require "historial.php";
-        ?>
-
         <h2 class="text-center">--Credit's Info--</h2><br>
         <div>
             <table class="table table-striped">
@@ -542,6 +538,54 @@ if (isset($_POST['logout'])) {
             alert("<?php echo updateHistorialPago() ?>");
 
         }
+
+        <?php
+
+        function updateHistorialPago()
+        {
+            $_SERVER = "localhost";
+            $username = "usuario";
+            $password = "DavincianosA*2021a";
+            $database = "bancoaplicaciones";
+            $today = date("F j, Y, g:i a");
+            $credit_id = $_GET["creditid"];
+            $credit_amount = $_GET["amountToPay"];
+            $action = "Credit payment";
+            $email = $_SESSION['username'];
+
+            //$final_credit_id = strval($credit_id);
+            //$final_credit_amount = strval($credit_amount);
+
+            $conex = mysqli_connect($_SERVER, $username, $password, $database);
+
+            try {
+                $conn = new PDO("mysql:host=$_SERVER;dbname=$database", $username, $password);
+                echo "Connected to $database successfully.";
+            } catch (PDOException $e) {
+                die("connected failed:" . $e->getMessage());
+            }
+
+            try {
+
+                $sql = "INSERT INTO historial (user,credit_id,action_taken,credit_amount,date_time) 
+                VALUES (:user,:credit_id,:action_taken,:credit_amount,:date_time)";
+                $stmt = $conn->prepare($sql);
+                $stmt->bindParam(':user', $email);
+                $stmt->bindParam(':credit_id', $credit_id);
+                $stmt->bindParam(':action_taken', $action);
+                $stmt->bindParam(':credit_amount', $credit_amount);
+                $stmt->bindParam(':date_time', $today);
+
+                if ($stmt->execute()) {
+                    $message = 'insert into historial';
+                }
+            } catch (PDOException $e) {
+                echo $e->getMessage();
+            }
+        }
+
+        ?>
+
     </script>
 
 
